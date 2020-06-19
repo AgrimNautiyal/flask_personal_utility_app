15a40546d8d0fe26cccebbb3882d443dfa9a185d
unknown <agrimnautiyal1999@gmail.com>
2020-06-20 01:55:31 +0530

    Enabled session tracking - testing remaining

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, jsonify, flash
 from wtforms import Form, TextAreaField, validators
 from dotenv import load_dotenv
 from datetime import datetime
+import requests
 import sqlite3
 import json
-import requests
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+conn = sqlite3.connect('users.db')
+
 
 #HOME PAGE
 @app.route('/')
@@ -26,7 +34,23 @@ def signup():
     return render_template('signup.html')
 @app.route('/signup_verify', methods=['POST'])
 def signup_verify():
-    pass
+    Name = request.form['Name']
+    Email = request.form['Email']
+    Pass = request.form['Password']
+    ConfPass = request.form['ConfPassword']
+    if Pass != ConfPass:
+        #non matching passwords ---> need to add in a prompt on webpage
+        return redirect(url_for('signup'))
+    hashedPassword = generate_password_hash(Pass, method='sha256')
+    Desc = request.form['Desc']
+    #now to add values into the database
+    with sqlite3.connect('users.db') as con:
+        cur = con.cursor()
+        cur.execute('''INSERT INTO EnrolledUsers VALUES(?,?,?,?)''', (Name, Email, hashedPassword, Desc))
+        con.commit()
+
+    return render_template('login.html', Prompt_For_Login = "Sign up successful! Please login to continue.")
+    #return render_template('login.html')
 
 #LOGIN ROUTES
 @app.route('/login')
@@ -34,8 +58,21 @@ def login():
     return render_template('login.html')
 @app.route('/login_verify', methods=['POST'])
 def login_verify():
-    pass
+    return 1
 
+
+#DASHBOARD ROUTES
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+#LOGOUT ROUTES
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ dbtest.py
@@ -0,0 +1,22 @@
+import sqlite3
+#conn = sqlite3.connect('users.db')
+#print("Connection established!")
+
+#cur = conn.cursor()
+
+#cur.execute('''INSERT INTO EnrolledUsers values('Agrim', 'ag@in.com', 'password123', 'Cool')''')
+#conn.commit()
+#print("Done")
+
+
+
+
+
+with sqlite3.connect('users.db') as con:
+    cur=con.cursor()
+    cur.execute('SELECT*FROM EnrolledUsers')
+    rows=cur.fetchall()
+    for row in rows:
+        print(row)
+
+
